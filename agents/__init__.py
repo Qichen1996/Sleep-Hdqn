225b26e51a9ad589b6c979d567a2a75867c01546
Qichen1996 <72041979+Qichen1996@users.noreply.github.com>
2024-02-09 22:36:19 +0100

    Add files via upload

---
+++ agents/__init__.py
@@ -0,0 +1,4 @@
+from .mappo import MappoPolicy
+from .random import RandomPolicy
+from .fixed import AlwaysOnPolicy
+from .simple import SimplePolicy, SimplePolicySM1Only, SimplePolicyNoSM3, SleepyPolicy
